ffaa6bee1033b4090a427ecbcf1d6e834ac81479
Robert Vigil <rvigil@roylu.com>
2024-11-25 17:10:13 +0000

    latest

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+git add .
+git commit -m 'latest'
+git push -u origin main
--- images/build-favicon.sh
+++ images/build-favicon.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 convert favicon.svg -define icon:auto-resize=16,32,48 favicon.ico
